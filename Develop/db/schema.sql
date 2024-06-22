c897c4dd4809b09d92fedb93b9ae91bce21edb1e
Owen Kanzler <oj.kanz1@gmail.com>
2024-06-22 15:13:47 -0500

    fixed errors from previous submission

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,25 +1,25 @@
-DROP DATABASE IF EXISTS challenege10_db;
-CREATE DATABASE challenege10_db;
+DROP DATABASE IF EXISTS challenge10_db;
+CREATE DATABASE challenge10_db;
 
 CREATE TABLE department (
-    department_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     department_name VARCHAR(30)
 );
 
 CREATE TABLE employee_role (
-    role_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(10, 2),
     department_id INTEGER,
-    FOREIGN KEY (department_id) REFERENCES department(department_id)
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    employee_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
     manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES employee_role(role_id),
-    FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
+    FOREIGN KEY (role_id) REFERENCES employee_role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
